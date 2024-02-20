05239e1ecd3c5d700cf4b70b1ee1bc65fc8f088c
Vikrant Gupta <vikrant.thomso@gmail.com>
2024-02-20 14:50:10 +0530

    fix: build issues

--- lib/actions/category.actions.ts
+++ lib/actions/category.actions.ts
@@ -11,40 +11,46 @@ const tracer = trace.getTracer("categoryActions");
 export const createCategory = async ({
   categoryName,
 }: CreateCategoryParams) => {
-  tracer.startActiveSpan("categoryActions:createCategory", async (span) => {
-    try {
-      await connectToDatabase();
+  return tracer.startActiveSpan(
+    "categoryActions:createCategory",
+    async (span) => {
+      try {
+        await connectToDatabase();
 
-      const newCategory = await Category.create({ name: categoryName });
+        const newCategory = await Category.create({ name: categoryName });
 
-      span.setAttribute("categoryName", categoryName);
+        span.setAttribute("categoryName", categoryName);
 
-      return JSON.parse(JSON.stringify(newCategory));
-    } catch (error) {
-      span.setAttribute("createCategoryException", "something went wrong");
-      span.recordException(String(error));
-      handleError(error);
-    } finally {
-      span.end();
+        return JSON.parse(JSON.stringify(newCategory));
+      } catch (error) {
+        span.setAttribute("createCategoryException", "something went wrong");
+        span.recordException(String(error));
+        handleError(error);
+      } finally {
+        span.end();
+      }
     }
-  });
+  );
 };
 
 export const getAllCategories = async () => {
-  tracer.startActiveSpan("categoryAction:getAllCategories", async (span) => {
-    try {
-      await connectToDatabase();
+  return tracer.startActiveSpan(
+    "categoryAction:getAllCategories",
+    async (span) => {
+      try {
+        await connectToDatabase();
 
-      const categories = await Category.find();
+        const categories = await Category.find();
 
-      span.setAttribute("fetchedCategories", "categories fetched from DB");
+        span.setAttribute("fetchedCategories", "categories fetched from DB");
 
-      return JSON.parse(JSON.stringify(categories));
-    } catch (error) {
-      span.recordException(String(error));
-      handleError(error);
-    } finally {
-      span.end();
+        return JSON.parse(JSON.stringify(categories));
+      } catch (error) {
+        span.recordException(String(error));
+        handleError(error);
+      } finally {
+        span.end();
+      }
     }
-  });
+  );
 };
